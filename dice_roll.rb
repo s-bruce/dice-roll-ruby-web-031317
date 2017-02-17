409eb461c27b3f6928e02298b36872ff1ae16596
github username <github email address>
2017-02-17 17:55:17 -0500

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,13 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#First I solved this using a range
+#def roll
+ # rand(1..6)
+#end
+
 def roll
-  # code goes here
+  array = [1, 2, 3, 4, 5, 6]
+  max = array.length
+  array[rand(max)]
 end
